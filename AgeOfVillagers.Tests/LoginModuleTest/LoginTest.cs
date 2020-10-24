a56cdc07b4d633ad15df33793a5f5a0cf3412c20
JAZZHAND1 <rotorblade52@outlook.com>
2020-10-24 16:14:47 +0600

    1st commit

---
+++ AgeOfVillagers.Tests/LoginModuleTest/LoginTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace AgeOfVillagers.Tests.LoginModule
+{
+    public class LoginTest
+    {
+       
+    }
+}
---
+++ Login/User_Credentials.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Login
+{
+    class User_Credentials
+    {
+        private static int generateID = 0;
+        private int ID;
+        private string username;
+        private string password;
+
+        public User_Credentials()
+        {
+            generateID = generateID + 1;
+        }
+
+        public int return_ID()
+        {
+            return generateID;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public int ID1 { get => ID; set => ID = value; }
+    }
+
+}
